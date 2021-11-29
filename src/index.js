d147158252d13936cb311da12ae7a35037a6025a
bert <herbertmatos10@hotmail.com>
2021-11-29 20:58:03 -0300

    feat: implementa a criacao de conta e validacao de cpf para permitir criacao

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
+
+const customers = require('./mocks/customers');
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body;
+
+  const custumerAlreadyExists = customers.some((customer) => (
+    customer.cpf === cpf
+  ));
+  if (custumerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer already exists!' });
+  }
+
+  customers.push({
+    name,
+    cpf,
+    id: uuidV4(),
+    statement: []
+  });
+
+  return response.sendStatus(201);
+});
 
 app.listen(3000);
